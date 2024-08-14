1051472187f346f471a11530ff9ff85e125c41ba
Shahwar <itisshahwar@gmail.com>
2024-08-14 02:20:39 +0100

    one to many association added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  before_save { self.email = email.downcase }
   has_many :articles
 
   validates :username, presence: true, uniqueness: true, length: { minimum: 3, maximum: 10 }
